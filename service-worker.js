f8ccaa9832285b2940099a756bad4182b08a3943
pongoj <j.pongo@pon-go.hu>
2026-02-24 10:32:28 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ self.addEventListener("message", (event) => {
 
 // CACHE VERSION: ezt és az APP_VERSION-t együtt növeld!
 // Pl: APP_VERSION = "0.4.1" és itt: CACHE_VERSION = "v0.4.1"
-const CACHE_VERSION = "v5.24.6";
+const CACHE_VERSION = "v5.24.7";
 const CACHE_NAME = `citymap-cache-${CACHE_VERSION}`;
 
 const CORE = [
@@ -16,8 +16,7 @@ const CORE = [
   "./manifest.json",
   "./service-worker.js",
   "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./icons/apple-touch-icon.png"
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {

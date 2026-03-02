57e742bf10d49d01d85cefb89edfc9e3f74f69ce
pongoj <j.pongo@pon-go.hu>
2026-03-02 19:27:42 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -5,8 +5,8 @@ self.addEventListener("message", (event) => {
 });
 
 // CACHE VERSION: ezt és az APP_VERSION-t együtt növeld!
-// Pl: APP_VERSION = "5.31" és itt: CACHE_VERSION = "v5.38"
-const CACHE_VERSION = "v5.38";
+// Pl: APP_VERSION = "5.31" és itt: CACHE_VERSION = "v5.38.1"
+const CACHE_VERSION = "v5.38.1";
 const CACHE_NAME = `citymap-cache-${CACHE_VERSION}`;
 
 const CORE = [
